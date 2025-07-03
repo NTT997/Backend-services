4b45295b1f3399a519bb1dab601d6d0621f83212
TNT97 <truongtho2907@gmail.com>
2025-07-03 16:07:39 +0700

    "#16 @getByFriendlyUrl method in productrepositoryimpl. Add additional logic to make it throw right error"

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/ProductRepositoryImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/ProductRepositoryImpl.java
@@ -259,6 +259,9 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 				LOGGER.error("Found multiple products for list of criterias with main criteria [" + seUrl + "]");
 			}
 			// p = (Product)q.getSingleResult();
+			
+			if(products.isEmpty()) return p;
+			
 			p = products.get(0);
 		} catch (javax.persistence.NoResultException ignore) {
 
