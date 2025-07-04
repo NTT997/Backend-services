7312f1652b223c6082da30f03b9cc81da8cd6db9
TNT97 <truongtho2907@gmail.com>
2025-07-04 09:35:04 +0700

    "#16 @SalesManagerEntityServiceImpl: change the method getById(k id) => return repository.findById(id).orElse(null) to have better error response @ProductCommonFacadeImpl: fix spelling in the message of RNF exception"

--- sm-shop/src/main/java/com/salesmanager/shop/store/facade/product/ProductCommonFacadeImpl.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/facade/product/ProductCommonFacadeImpl.java
@@ -393,12 +393,12 @@ public class ProductCommonFacadeImpl implements ProductCommonFacade {
 		Product p = productService.getById(id);
 
 		if (p == null) {
-			throw new ResourceNotFoundException("Product with id [" + id + " not found");
+			throw new ResourceNotFoundException("Product with id [" + id + "] not found");
 		}
 
 		if (p.getMerchantStore().getId().intValue() != store.getId().intValue()) {
 			throw new ResourceNotFoundException(
-					"Product with id [" + id + " not found for store [" + store.getCode() + "]");
+					"Product with id [" + id + "] not found for store [" + store.getCode() + "]");
 		}
 
 		try {
